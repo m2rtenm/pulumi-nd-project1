4ebac8ff889d0b40d3515257af943926d459c1f1
Märten Mändla <marten.mandla@gmail.com>
2024-06-17 21:26:49 +0300

    Network partially done

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Infrastructure as a Code with Pulumi in Azure and AWS
 
 The purpose of this repository is to demonstrate examples how to deploy infrastructure with Pulumi to Azure and AWS. The inspiration was taken from the Cloud DevOps nanodegree by Udacity. During the nanodegree, I completed the first IaaC project using Terraform to deploy infrastructure in Azure. I adapted the setup and made a similar approach in AWS. This repo helps to present the way how the deployment can be done using Pulumi.
+
+## Prerequisites
+
+* Install Pulumi to your local machine:
+
+````
+curl -fsSL https://get.pulumi.com | sh
+````
+
+* Configure your [Pulumi user](https://www.pulumi.com/docs/get-started/)
--- azure/python/__main__.py
+++ azure/python/__main__.py
@@ -1,31 +1,69 @@
 """An Azure RM Python Pulumi program"""
 
 import pulumi
-from pulumi_azure_native import storage
+from pulumi_azure_native import network
 from pulumi_azure_native import resources
+from pulumi_azure_native import compute
 
-# Create an Azure Resource Group
-resource_group = resources.ResourceGroup("resource_group")
+# Config and variables
+config = pulumi.Config()
 
-# Create an Azure resource (Storage Account)
-account = storage.StorageAccount(
-    "sa",
-    resource_group_name=resource_group.name,
-    sku=storage.SkuArgs(
-        name=storage.SkuName.STANDARD_LRS,
-    ),
-    kind=storage.Kind.STORAGE_V2,
+prefix = config.get("prefix")
+location = config.get("location")
+counter = config.get_int("counter")
+username = config.get("username")
+vm_size = config.get("vm_size")
+disk_size = config.get_int("disk_size")
+
+# Resource group
+resource_group = resources.ResourceGroup(
+    resource_name=f"{prefix}-rg",
+    location=location
 )
 
-# Export the primary key of the Storage Account
-primary_key = (
-    pulumi.Output.all(resource_group.name, account.name)
-    .apply(
-        lambda args: storage.list_storage_account_keys(
-            resource_group_name=args[0], account_name=args[1]
-        )
+# Virtual network
+virtual_network = network.VirtualNetwork(
+    resource_name=f"{prefix}-network",
+    address_spaces=network.AddressSpaceArgs(
+        address_prefixes=["10.0.0.0/16"]
     )
-    .apply(lambda accountKeys: accountKeys.keys[0].value)
 )
 
-pulumi.export("primary_storage_key", primary_key)
+# Subnet
+subnet = network.Subnet(
+    resource_name="internal",
+    resource_group_name=resource_group.name,
+    virtual_network_name=virtual_network.name,
+    address_prefixes=["10.0.0.0/24"]
+)
+
+# Network Security Group
+nsg = network.NetworkSecurityGroup(
+    resource_name=f"{prefix}-nsg",
+    resource_group_name=resource_group.name,
+    security_rules=[
+        network.SecurityRuleArgs(
+            name="AllowInternalTraffic",
+            priority=100,
+            direction=network.SecurityRuleDirection.INBOUND,
+            access=network.SecurityRuleAccess.ALLOW,
+            protocol=network.SecurityRuleProtocol.TCP,
+            source_address_prefix=[subnet.address_prefixes[0]],
+            source_port_range="*",
+            destination_address_prefix=[subnet.address_prefixes[0]],
+            destination_port_range="*"
+        ),
+        network.SecurityRuleArgs(
+            name="DenyInternetInbound",
+            priority=200,
+            direction=network.SecurityRuleDirection.INBOUND,
+            access=network.SecurityRuleAccess.DENY,
+            protocol=network.SecurityRuleProtocol.TCP,
+            source_address_prefix="Internet",
+            source_port_range="*",
+            destination_address_prefix="*",
+            destination_port_range="*"
+        )
+    ]
+)
+
